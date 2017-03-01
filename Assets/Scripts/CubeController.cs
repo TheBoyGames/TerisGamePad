411a92bfc907dad868ad39a7f6c70af22cfd7820
TheBoy <theboy5140@gmail.com>
2017-03-01 18:37:54 +0800

    add cube controller

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    public float fallDownSpeed;
+    public float fallDownStep;
+
+    IEnumerator FallDown()
+    {
+        while (true) {
+            Vector3 currentPosition = transform.position;
+            Vector3 step = new Vector3 (0.0f, -1 * 1 * fallDownStep, 0.0f);
+            transform.position = currentPosition + step;
+            yield return new WaitForSeconds (fallDownSpeed);
+            Debug.Log ("yield");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine (FallDown());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject cube;
+
+    public int cubeCount = 1;
+
+    // 10 * 12 cubes
+    public void SpawnCubes()
+    {
+        Instantiate (cube, new Vector3(4.0f, 0f, 0f), Quaternion.identity);
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
